6ea316ac22b293132e4b4244144d0f1524c489de
MatthewChalkG <105702497+MatthewChalkG@users.noreply.github.com>
2023-03-11 12:24:35 -0800

    .

--- sortedelectricaldata/datalogging/HallEffect.py
+++ sortedelectricaldata/datalogging/HallEffect.py
@@ -10,7 +10,7 @@ from MachineCode.arduinorelayinterface import Arduino
 timeStamp = str(time.time())[3:10]
 fn = "hallVoltageSweep{}.txt".format(timeStamp)
 f = open("Data/"+fn, "a")
-f.write("t,i,x,y,r,theta,xK,tc,therm,dc\n")
+f.write("t,i,x,y,r,theta,xK\n")
 f.close()
 
 LIA = SR2124.SR2124('COM5')
@@ -21,7 +21,7 @@ SPD3303x.set_voltage(5)
 SPD3303x.set_current(0)
 
 
-
+startTime = time.time() 
 for direction in [(0, 3.2), (3.2, 0), (0, -3.2), (-3.2,0)]: 
     for i in np.linspace(direction[0], direction[1], 20):
         if direction[0] > -1 and direction[1] > -1:
@@ -36,9 +36,9 @@ for direction in [(0, 3.2), (3.2, 0), (0, -3.2), (-3.2,0)]:
         lockstatus = LIA.readlock()
         xK = 0
         f = open("Data/"+fn, "a")
-
-        print("i: {}, x: {}, y: {}, r: {}, theta: {}, xK: {}".format(i, x, y, r, theta, xK))
-        f.write(str(i) + ',' + str(x)+',' + str(y) + ',' + str(r) + ',' + str(theta) + ',' + str(xK) + "\n")
+        t = time.time()
+        print("t: {}, i: {}, x: {}, y: {}, r: {}, theta: {}, xK: {}".format(t-startTime, i, x, y, r, theta, xK))
+        f.write(str(t) + ',' + str(i) + ',' + str(x)+',' + str(y) + ',' + str(r) + ',' + str(theta) + ',' + str(xK) + "\n")
         f.close()
 
 
