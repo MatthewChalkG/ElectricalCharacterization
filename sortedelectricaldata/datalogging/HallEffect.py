4204320da82f2b65d25a687987660815dfa54c07
MatthewChalkG <105702497+MatthewChalkG@users.noreply.github.com>
2023-03-11 12:08:20 -0800

    .

--- sortedelectricaldata/datalogging/HallEffect.py
+++ sortedelectricaldata/datalogging/HallEffect.py
@@ -1,4 +1,4 @@
-import MachineCode.SR2124
+from  MachineCode import SR2124
 from MachineCode.SPD3303X import spd3303x
 import numpy as np
 import time
@@ -7,15 +7,14 @@ from MachineCode.keithley2110tc import keithley2110tc
 from MachineCode.arduinorelayinterface import Arduino
 
 fn = "hallVoltage.txt"
-os.remove(fn)
-LIA = SR2124.SR2124('COM9')
+#os.remove(fn)
+LIA = SR2124.SR2124('COM5')
 SPD3303x = spd3303x()
 f = open(fn, "a")
 f.write("i,x,y,r,theta,xK\n")
 f.close()
 SPD3303x.set_voltage(5)
 SPD3303x.set_current(0)
-keith = keithley2110tc()
 relay = Arduino("COM3")
 
 while True:
@@ -31,10 +30,10 @@ while True:
             x, y, r, theta = LIA.readall() 
 
             lockstatus = LIA.readlock()
-            xK = keith.voltage()
+            xK = 0
             f = open(fn, "a")
 
-            print("i: {}, x: {}, y: {}, r: {}, theta: {}, xK: {}, sens: {}".format(i, x, y, r, theta, xK))
+            print("i: {}, x: {}, y: {}, r: {}, theta: {}, xK: {}".format(i, x, y, r, theta, xK))
             f.write(str(i) + ',' + str(x)+',' + str(y) + ',' + str(r) + ',' + str(theta) + ',' + str(xK) + "\n")
             f.close()
 
