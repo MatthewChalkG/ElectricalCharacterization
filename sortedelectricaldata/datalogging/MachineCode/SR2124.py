a4e3fe1bf47e33d1226a1e6bbd86f4b75b0156a1
MatthewChalkG <105702497+MatthewChalkG@users.noreply.github.com>
2023-03-13 02:00:13 -0700

    .

--- sortedelectricaldata/datalogging/MachineCode/SR2124.py
+++ sortedelectricaldata/datalogging/MachineCode/SR2124.py
@@ -97,20 +97,20 @@ class SR2124:
         
         if ovld == 8:
             self.autoOffsetX()
-            time.sleep(12)
+            time.sleep(15)
 
         elif ovld == 16:
             self.autoOffsetY()
-            time.sleep(12)
+            time.sleep(15)
 
         elif ovld == 24:
             self.autoOffsetX()
-            time.sleep(12)
+            time.sleep(15)
             self.autoOffsetY()
-            time.sleep(12)
+            time.sleep(15)
         
         elif ovld != 0:
             self.autoOffsetX()
-            time.sleep(12)
+            time.sleep(15)
             self.autoOffsetY()
-            time.sleep(12)
+            time.sleep(15)
